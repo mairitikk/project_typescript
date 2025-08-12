82cd02befdaadda44380c0c62b0f8065b2cb44c3
mairitikk <mairi.tikk@gmail.com>
2025-08-12 14:24:09 +0200

    changes Child

--- rts/src/props/Child.tsx
+++ rts/src/props/Child.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 interface ChildProps{
     color: string;
+    onClick: () => void;
+  children?: React.ReactNode;
 }
 export const Child = ({color}:ChildProps) => {
   return (
