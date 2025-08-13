d479ad3afa96a6d4f60648cc5a34c6b076b5095b
mairitikk <mairi.tikk@gmail.com>
2025-08-13 09:27:20 +0200

    add other button

--- rts/src/props/Child.tsx
+++ rts/src/props/Child.tsx
@@ -7,12 +7,14 @@ interface ChildProps{
 export const Child = ({color}:ChildProps) => {
   return (
     <div>{color}
-    <button>Click me</button>
+    <button onClick={onClick}>Click me</button>
     </div>
   )
 }
 // parem variant kirjutada, siis pakub ise variante v2lja
 export const ChildAsFC: React.FC<ChildProps> = ({color}) => {
-    return <div> {color}</div> 
+    return <div> {color}
+    <button onClick={onClick}>Click me</button>
+    </div> 
 };
 ChildAsFC.displayName
