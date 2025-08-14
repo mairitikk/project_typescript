bad8db229de357343a31bea4d5b9854e632ff3b5
mairitikk <mairi.tikk@gmail.com>
2025-08-14 14:05:13 +0200

    add input and button

--- rts/src/state/GuestList.tsx
+++ rts/src/state/GuestList.tsx
@@ -1,4 +1,9 @@
 const GuestList: React.FC = () => {
-    return <div>Guest List</div>
+    return <div>
+       <h3>Guest List</h3> 
+       <input type="text" />
+       <button></button>
+
+        </div>
 }
 export default GuestList;
