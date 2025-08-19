0ab50f1fda876cf8416948d78d2207148d84ea94
mairitikk <mairi.tikk@gmail.com>
2025-08-19 09:39:51 +0200

    list changes

--- rts/src/state/GuestList.tsx
+++ rts/src/state/GuestList.tsx
@@ -5,13 +5,14 @@ const GuestList: React.FC = () => {
 const [name, setName] = useState('');
 const [guests, setGuests] = useState([]);
 const onClick = () => {
+    setName('');
 
 };
 
     return <div>
        <h3>Guest List</h3> 
        <input type="text" value={name} onChange={(e) => setName(e.target.value)}  />
-       <button onClick={}> Add Guest</button>
+       <button onClick={onClick}> Add Guest</button>
 
         </div>
 }
