840a5abc04073e1030a67b8825175c8cfd2e58f2
mairitikk <mairi.tikk@gmail.com>
2025-08-20 10:21:37 +0200

    add mapp to guestlist

--- rts/src/state/GuestList.tsx
+++ rts/src/state/GuestList.tsx
@@ -12,6 +12,11 @@ const onClick = () => {
 
     return <div>
        <h3>Guest List</h3> 
+       <ul>
+        {guests.map((guest)=> (
+            <li key={guest}>{guest} </li>
+            ))}
+       </ul>
        <input type="text" value={name} onChange={(e) => setName(e.target.value)}  />
        <button onClick={onClick}> Add Guest</button>
 
