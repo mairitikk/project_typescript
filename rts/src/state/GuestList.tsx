2567ead4b330d8c3dc412873d9c0d64c2dd92103
mairitikk <mairi.tikk@gmail.com>
2025-08-20 09:51:57 +0200

    state changes

--- rts/src/state/GuestList.tsx
+++ rts/src/state/GuestList.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 
 const GuestList: React.FC = () => {
 const [name, setName] = useState('');
-const [guests, setGuests] = useState([]);
+const [guests, setGuests] = useState<string[]>([]);
 const onClick = () => {
     setName('');
+    setGuests([...guests, name])
 
 };
 
